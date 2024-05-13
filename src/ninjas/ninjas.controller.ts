88bde4810d9415dd16f727819ef1126d6c7e6afa
Muhammad Amit Hasan <bhamithasan@gmail.com>
2024-05-13 12:25:02 +0600

    CRUD API

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,34 +1,61 @@
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
+import { NinjasService } from './ninjas.service';
+
+// behind the scenes whats happing is that the controller is calling the service
+// const service = new NinjasService();
+// const controller = new NinjasController(service);
 
 @Controller('ninjas')
 export class NinjasController {
-  // GET /ninjas
+  // create constructor to not calling every time the service
+  constructor(private readonly ninjasService: NinjasService) {}
+
+  // GET /ninjas --> []
+  // @Get()
+  // getNinjas() {
+  //   return [];
+  // }
+
+  // GET /ninjas?weapon=fast --> []
   @Get()
-  getNinjas() {
-    return [];
+  getNinjas(@Query('weapon') weapon: 'stars' | 'nunchucks') {
+    // const service = new NinjasService();
+    return this.ninjasService.getNinjas(weapon);
   }
 
   // GET /ninjas/:id
   @Get(':id')
-  getSingleNinja() {
-    return {};
+  // get the id of the ninja from the URL
+  getSingleNinja(@Param('id') id: string) {
+    return this.ninjasService.getNinja(Number(id));
   }
 
   // POST /ninjas
   @Post()
-  createNinja() {
-    return {};
+  // Take body from API request
+  createNinja(@Body() createNinjaDto: CreateNinjaDto) {
+    return this.ninjasService.createNinja(createNinjaDto);
   }
 
   //PUT /ninjas/:id
   @Put(':id')
-  updateNinja() {
-    return {};
+  updateNinja(@Param('id') id: string, @Body() updateNinjaDto: CreateNinjaDto) {
+    return this.ninjasService.updateNinja(Number(id), updateNinjaDto);
   }
 
   // DELETE /ninjas/:id
   @Delete(':id')
-  deleteNinja() {
-    return {};
+  deleteNinja(@Param('id') id: string) {
+    return this.ninjasService.removeNinja(Number(id));
   }
 }
--- src/ninjas/ninjas.service.ts
+++ src/ninjas/ninjas.service.ts
@@ -1,4 +1,52 @@
 import { Injectable } from '@nestjs/common';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
 
 @Injectable()
-export class NinjasService {}
+export class NinjasService {
+  private ninjas = [
+    { id: 1, name: 'Ryu', weapon: 'stars' },
+    { id: 2, name: 'Ken', weapon: 'nunchucks' },
+  ];
+
+  getNinjas(weapon?: 'stars' | 'nunchucks') {
+    if (weapon) {
+      return this.ninjas.filter((ninja) => ninja.weapon === weapon);
+    }
+    return this.ninjas;
+  }
+
+  getNinja(id: number) {
+    const ninja = this.ninjas?.find((ninja) => ninja?.id === id);
+    if (!ninja) {
+      throw new Error('Ninja not found');
+    } else return ninja;
+  }
+
+  createNinja(createNinjaDto: CreateNinjaDto) {
+    const newNinja = {
+      ...createNinjaDto,
+      id: Date.now(),
+    };
+    this.ninjas.push(newNinja);
+    return newNinja;
+  }
+
+  updateNinja(id: number, updateNinjaDto: CreateNinjaDto) {
+    this.ninjas = this.ninjas.map((ninja) => {
+      if (ninja.id === id) {
+        return {
+          ...ninja,
+          ...updateNinjaDto,
+        };
+      }
+      return ninja;
+    });
+    return this.getNinja(id);
+  }
+
+  removeNinja(id: number) {
+    const toBeRemoved = this.getNinja(id);
+    this.ninjas = this.ninjas.filter((ninja) => ninja.id !== id);
+    return toBeRemoved;
+  }
+}
